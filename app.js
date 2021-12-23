0ad70530805546519c4d7a31846be6f62e72fba7
pdguerra12 <paul.guerra09@gmail.com>
2021-12-23 17:08:16 -0600

    feature/questions updates

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
-const fs = require("fs");
-const generatePage = require("./src/page-template.js");
+const inquirer = require("inquirer");
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+	.prompt([
+		{
+			type: "input",
+			name: "name",
+			message: "What is your name?",
+		},
+	])
+	.then((answers) => console.log(answers));
+// const fs = require("fs");
+// const generatePage = require("./src/page-template");
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile("./index.html", generatePage(name, github), (err) => {
-	if (err) throw new Error(err);
+// fs.writeFile("./index.html", pageHTML, (err) => {
+// 	if (err) throw err;
 
-	console.log("Portfolio complete! Check out index.html to see the output!");
-});
+// 	console.log("Portfolio complete! Check out index.html to see the output!");
+// });
 
-// // Notice the lack of parentheses around the `profileDataArr` parameter?
-// const printProfileData = (profileDataArr) => {
-// 	// This...
-// 	for (let i = 0; i < profileDataArr.length; i += 1) {
-// 		console.log(profileDataArr[i]);
-// 	}
+// *** Commenting below doesn't below to the above code and left for future reference ***
 
-// 	console.log("================");
+// // // Notice the lack of parentheses around the `profileDataArr` parameter?
+// // const printProfileData = (profileDataArr) => {
+// // 	// This...
+// // 	for (let i = 0; i < profileDataArr.length; i += 1) {
+// // 		console.log(profileDataArr[i]);
+// // 	}
 
-// 	// Is the same as this...
-// 	// profileDataArr.forEach((profileItem) => {
-// 	// 	console.log(profileItem);
-// 	// });
-// 	profileDataArr.forEach((profileItem) => console.log(profileItem));
-// };
+// // 	console.log("================");
 
-// printProfileData(profileDataArgs);
+// // 	// Is the same as this...
+// // 	// profileDataArr.forEach((profileItem) => {
+// // 	// 	console.log(profileItem);
+// // 	// });
+// // 	profileDataArr.forEach((profileItem) => console.log(profileItem));
+// // };
+
+// // printProfileData(profileDataArgs);
