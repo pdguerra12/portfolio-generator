399193f396b49b906d9981c3048c4f02dfd21d55
pdguerra12 <paul.guerra09@gmail.com>
2021-12-28 16:34:54 -0600

    template updated

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+const generatePage = require("./src/page-template");
 
 const promptUser = () => {
 	return inquirer.prompt([
@@ -146,24 +148,16 @@ const promptProject = (portfolioData) => {
 promptUser()
 	.then(promptProject)
 	.then((portfolioData) => {
-		console.log(portfolioData);
+		// .then((mockData) => {
+		// const pageHTML = generatePage(mockData);
+		const pageHTML = generatePage(portfolioData);
+		fs.writeFile("./index.html", pageHTML, (err) => {
+			if (err) throw new Error(err);
+			console.log(
+				"Page created! Check out index.html in this directory to see it!"
+			);
+		});
 	});
-
-// promptUser()
-// 	.then((answers) => console.log(answers))
-// 	.then(promptProject)
-// 	.then((projectAnswers) => console.log(projectAnswers));
-
-// const fs = require("fs");
-// const generatePage = require("./src/page-template");
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile("./index.html", pageHTML, (err) => {
-// 	if (err) throw err;
-
-// 	console.log("Portfolio complete! Check out index.html to see the output!");
-// });
 
 // *** Commenting below doesn't below to the above code and left for future reference ***
 
